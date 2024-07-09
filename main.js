f4909723843c0b004e5a9980e2dc567487df4791
SantiagoGonzalez1206 <160767943+SantiagoGonzalez1206@users.noreply.github.com>
2024-07-09 06:40:41 -0500

    feat: :sparkles: ejercicio 2
    Crea una clase llamada Estudiante que herede de la clase Persona. Agrega una nueva propiedad llamada carrera y un método llamado estudiar que imprima en la consola un mensajeindicandolacarreraqueestáestudiando. Crea una instancia de la clase Estudiantellamada estudiante1 y asigna valores a sus propiedades. Luego, llama al método saludar() para que el estudiante imprima su saludo y al método estudiar() para que el estudiante imprima el mensaje de su carrera.

--- main.js
+++ main.js
@@ -11,3 +11,17 @@ class Persona {
 }
 const persona1= new Persona ("santi", 23, "femenino")
 persona1.saludar()
+
+class Estudiante extends Persona{
+    constructor(nombre, edad, sexo, carrera){
+        super(nombre, edad, sexo)
+        this.carrera = carrera
+    }
+
+    estudiar(){
+        console.log(`${this.nombre} esta en la carrera de ${this.carrera}`)
+    }
+}
+const estudiante1= new Estudiante ("Nicolle zarai", 23, "femenino", "Torrentismo")
+estudiante1.saludar()
+estudiante1.estudiar()
